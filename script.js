e3916a56a49b980211e527998572b3a53f690c9c
MUHAMMAD HUSSAIN SHAMIM <142653978+hussainshamim16@users.noreply.github.com>
2024-06-02 09:26:00 +0500

    only done

--- script.js
+++ script.js
@@ -23,30 +23,30 @@ var hours = 0;
 
 
 
-function en(){
+function en() {
     miniSecond++
-ms.innerHTML = miniSecond
+    ms.innerHTML = miniSecond
 
-if(miniSecond > 99){
+    if (miniSecond > 99) {
 
-    miniSecond = 0;   
-    second++ 
-sec.innerHTML = second
+        miniSecond = 0;
+        second++
+        sec.innerHTML = second
 
-}else if(second > 59){
-    second = 0;   
-    minutes++ 
-min.innerHTML = minutes;
-}
-else if(minutes > 59){
-    minutes = 0;   
-    hours++ 
-hour.innerHTML = hours;
+    } else if (second > 59) {
+        second = 0;
+        minutes++
+        min.innerHTML = minutes;
+    }
+    else if (minutes > 59) {
+        minutes = 0;
+        hours++
+        hour.innerHTML = hours;
 
-}
-if(hours > 12){
-    hours = 1;
-}
+    }
+    if (hours > 12) {
+        hours = 1;
+    }
 }
 
 
@@ -58,39 +58,48 @@ var duedate = 20;
 
 
 
-function start(hale){
-    time =   setInterval(en, 10);
+function start(abc) {
+    sta.style.display = "none";
+    sto.style.display = "inline";
+    res.style.display = "inline";
     watch.src = "icegif-699.gif";
-
-
-    
     console.log("start");
-  
+    time = setInterval(en, 10);
 }
 
 
 
 
 
-function stop(){
-    watch.src = "stop-removebg-preview.png";
-clearInterval(time)
-
-console.log("stop");
-
-}
-
-
-
-
-
-function reset(){
+function stop() {
     clearInterval(time)
-ms.innerHTML = "00"
-sec.innerHTML = "00"
-min.innerHTML = "00"
-hour.innerHTML = "00"
-watch.src = "stop-removebg-preview.png";
 
-console.log("reset",res.setAttribute("disabled",true));
+    sta.style.display = "inline";
+    sto.style.display = "none";
+    res.style.display = "inline";
+
+    watch.src = "stop-removebg-preview.png";
+    console.log("stop");
+
+}
+
+
+
+
+
+function reset() {
+    clearInterval(time)
+    // console.log("reset", res.setAttribute("disabled", true));
+
+    ms.innerHTML = "00"
+    sec.innerHTML = "00"
+    min.innerHTML = "00"
+    hour.innerHTML = "00"
+
+    watch.src = "stop-removebg-preview.png";
+
+
+    sta.style.display = "inline";
+    res.style.display = "none";
+    sto.style.display = "none";
 }
